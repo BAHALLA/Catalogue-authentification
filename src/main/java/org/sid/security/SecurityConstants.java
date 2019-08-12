bff3d1d684b166f1d407188e81701701ff67083f
bahalla <bahalla1997@gmail.com>
2019-08-13 00:18:45 +0200

    Initial commit

---
+++ src/main/java/org/sid/security/SecurityConstants.java
@@ -0,0 +1,7 @@
+package org.sid.security;
+
+public class SecurityConstants {
+    public static final String SECRET = "itJustMe";
+    public static final String HEADER_PREFIX = "Bearer ";
+    public static final String HEADER_AUTH = "Authorization";
+}
